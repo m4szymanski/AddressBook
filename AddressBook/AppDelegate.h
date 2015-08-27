4162c6a8fb06457a3933420af88269f9c100577b
Matt <matt@Matts-MacBook-Pro.local>
2015-08-26 21:36:08 -0400

    Initial Commit

---
+++ AddressBook/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AddressBook
+//
+//  Created by Matt on 2015-08-26.
+//  Copyright (c) 2015 Matthew Szymanski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
